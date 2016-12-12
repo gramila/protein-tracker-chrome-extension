d517f6e0f858e7e0b9796898573ba84865c1393b
Gema Beltran <gramila65@gmail.com>
2016-12-11 21:52:16 -0800

    adds options html and js files

---
+++ options.js
@@ -0,0 +1,16 @@
+$(function(){
+    chrome.storage.sync.get('goal', function(){
+        $('#goal').val(items.goal);
+    });
+    $('#save').click(function(){
+        var goal = $('#goal').val();
+        if(goal){
+            chrome.storage.sync.set({'goal': goal}, function(){
+                close();
+            });
+        }
+    });
+    $('#reset').click(function(){
+        chrome.storage.sync.set({'total': 0});
+    })
+})
