d69c0a426f8f54ab3d65c56563d88e00cdcf0f5f
Gema Beltran <gramila65@gmail.com>
2016-12-14 12:59:54 -0800

    adds functionality for context menu

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ $(function(){
         $('#goal').text(items.goal);
     });
     $('#addAmount').click(function(){
-        chrome.storage.sync.get('total', function(items){
+        chrome.storage.sync.get(['total', 'goal'], function(items){
             var newTotal = 0;
             if(items.total){
                 newTotal += parseInt(items.total);
@@ -18,6 +18,17 @@ $(function(){
             chrome.storage.sync.set({ 'total' : newTotal});
             $('#total').text(newTotal);
             $('#amount').val('');
+
+            if(newTotal >= items.goal){
+                var obj = {
+                    type: "basic",
+                    title: "Goal reached",
+                    message: "You reached your goal of " + items.goal + "!",
+                    iconUrl: "icon.png"
+                }
+                chrome.notifications.create('goalReached', obj, function(){})
+
+            }
         })
     });
 })
